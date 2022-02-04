6a40954c09a030075bf241025a313da3a21758cf
Maciej_Kurcius <maciej.kurcius@husarion.com>
2022-02-04 16:38:42 +0100

    Pixel Lib - update

---
+++ Examples/Arduino framework + STM32FreeRTOS/PixelLedCfg.cpp
@@ -0,0 +1,38 @@
+/**
+ * @file PixelLedCfg.cpp
+ * @author Maciej Kurcius
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-04
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <PixelLedCfg.h>
+
+SPIClass PixelSpi(PIXEL_MOSI, PB14, PIXEL_SCK);
+PixelLedClass PixelStrip(PIXEL_LENGTH);
+PixelStripSubsetClass RL_Light(&PixelStrip, 0, 4);
+PixelStripSubsetClass FL_Light(&PixelStrip, 5, 8);
+PixelStripSubsetClass FR_Light(&PixelStrip, 9, 12);
+PixelStripSubsetClass RR_Light(&PixelStrip, 13, 17);
+
+
+uint8_t PixelInitActions(void){
+    PixelStrip.PixelStripMapSwap(13,17);
+    PixelStrip.PixelStripMapSwap(14,16);
+}
+
+uint8_t PixelSpiInit(void){
+    SPISettings SpiSettings(PIXEL_SPI_SPEED, LSBFIRST, SPI_MODE3, SPI_TRANSMITONLY);
+    PixelSpi.beginTransaction(CS_PIN_CONTROLLED_BY_USER, SpiSettings);
+}
+
+void PixelSpiTransferData(uint8_t Data_){
+    PixelSpi.transfer(Data_);
+}
+
+void PixelDelay(uint16_t Time_){
+    vTaskDelay(Time_ / portTICK_PERIOD_MS);
+}
